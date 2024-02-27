86aee7ca082c57725837056f2c90c5eb61ea6eb0
Liger <gjfleo@yeah.net>
2024-02-27 14:24:56 +0800

    test: 添加单元测试

--- src/components/GLink.test.ts
+++ src/components/GLink.test.ts
@@ -1,14 +1,32 @@
-// import { shallowMount } from "@vue/test-utils";
+import { RouterLinkStub, mount } from "@vue/test-utils";
 
-// import GLink from "./GLink.vue";
+import GLink from "./GLink.vue";
 
-// describe("gLink", () => {
-//   it("链接文字正确", () => {
-//     const wrapper = shallowMount(GLink, { slots: { default: "Link Text" } });
-//     expect(wrapper.text()).toContain("Link Text");
-//   });
-//   it("链接跳转正确", () => {
-//     const wrapper = shallowMount(GLink, { slots: { default: "Link Text", href: "https://www.baidu.com" } });
-//     expect(wrapper.attributes().href).toEqual("https://www.baidu.com");
-//   });
-// });
+describe("gLink", () => {
+  it("链接文字", () => {
+    const wrapper = mount(GLink, {
+      slots: {
+        default: "Link Text",
+      },
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+        },
+      },
+    });
+    expect(wrapper.text()).toContain("Link Text");
+  });
+  it("链接跳转", () => {
+    const wrapper = mount(GLink, {
+      props: {
+        href: "https://www.baidu.com",
+      },
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+        },
+      },
+    });
+    expect(wrapper.attributes().href).toEqual("https://www.baidu.com");
+  });
+});
